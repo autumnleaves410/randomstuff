b36d142ee057e328abe50c58ea8c4dd1b1fee5dc
autumnleaves410 <114082214+autumnleaves410@users.noreply.github.com>
2023-09-12 23:25:19 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<VideoGame> listofGames = new List<VideoGame>(); //makes a new list for video games...referencing the video game class. 
+
+        Dictionary<int, VideoGame> gameDictionary = new Dictionary<int, VideoGame>();  //creates a int key and game object..using this to 
+
+        
+        string rootFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString(); //makes a new directory so that it can find the csv file easier?
+        string filePath = $"{rootFolder}{Path.DirectorySeparatorChar}videogames.csv";  //establishes a file path to videogames.csv
+
+        using (var sr = new StreamReader(filePath))
+        {
+            string[] fileReader = File.ReadAllLines(filePath);
+
+            
+
+            for (int gameNumber = 1; gameNumber < fileReader.Length; gameNumber++)
+            {
+                string line = fileReader[gameNumber];
+
+                string[] lineElements = line.Split(',');
+
+                VideoGame Games = new VideoGame
+                {
+                    Name = lineElements[0],
+                    Platform = lineElements[1],
+                    Genre = lineElements[3],
+                    Publisher = lineElements[4],
+    
+                };
+
+                gameDictionary.Add(gameNumber, Games);
+               
+
+                
+
+               try
+                {
+                    Console.WriteLine("Hey! Welcome to the Random Game Picker! How this is works is you pick a random number," +
+                        "and the number you pick is the game you'll play! So! Pick any number between 1 - 16328.");
+
+                    int userNum = Int32.Parse(Console.ReadLine());
+
+                    if (userNum < 1 || userNum > 16328)
+                    {
+                        Console.WriteLine("Enter a valid number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Great, you've chosen the number {userNum}");
+
+
+
+                        if (gameDictionary.TryGetValue(userNum, out VideoGame chosenGame))
+                        {
+
+                       
+
+
+                            Console.WriteLine("-----------------------");
+                            Console.WriteLine("Congrats! The game you'll be playing is..");
+
+                             Console.WriteLine($"Game: {chosenGame.Name}");
+                             Console.WriteLine($"Game Publisher: {chosenGame.Publisher}");
+                             Console.WriteLine($"Game Genre: {chosenGame.Genre}");
+                             Console.WriteLine($"Have fun!");
+                             Console.WriteLine("-----------------------"); 
+                            
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Game not found. There was a problem retrieving the data.");
+                        }
+
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("There was an error.");
+                } 
+
+                
+          
+
+            }
+
+            Console.WriteLine("Press Enter to Clear");
+            Console.Read();
+            Console.Clear();
+            
+        }
+
+       /* using (var sr = new StreamReader(filePath))
+        {
+            try
+            {
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error. Please try again.");
+            }
+        }*/
+      
+
+
+
+
+
+
+
+    }
+
+}
+
---
+++ VideoGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace Lab_2
+{
+    public class VideoGame : IComparable<VideoGame>
+    {
+        //Declare videogame.csv properties
+
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public double Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public decimal NA_Sales { get; set; }
+        public decimal EU_Sales { get; set; }
+        public decimal JP_Sales { get; set; }
+        public decimal Other_Sales { get; set; }
+        public decimal Global_Sales { get; set; }
+
+
+        //Default Constructor
+
+        public VideoGame()
+        {
+            Name = "N/A";
+            Platform = "N/A";
+            Year = 0;
+            Genre = "N/A";
+            Publisher = "N/A";
+            NA_Sales = 0;
+            EU_Sales = 0;
+            JP_Sales = 0;
+            Other_Sales = 0;
+            Global_Sales = 0;
+        }
+
+
+        //Copy Constructor
+
+        public VideoGame(string name, string platform, string year, string genre, string publisher, decimal na_Sales, decimal eu_Sales, decimal jp_Sales, decimal other_Sales, decimal global_Sales)
+        {
+            Name = this.Name;
+            Platform = this.Platform;
+            Year = this.Year;
+            Genre = this.Genre;
+            Publisher = this.Publisher;
+            NA_Sales = this.NA_Sales;
+            EU_Sales = this.EU_Sales;
+            JP_Sales = this.JP_Sales;
+            Other_Sales = this.Other_Sales;
+            Global_Sales = this.Global_Sales;
+
+        }
+
+        public VideoGame(string name, string platform, string genre, string publisher)
+        {
+            Name = this.Name;
+            Platform = this.Platform;
+            Genre = this.Genre;
+            Publisher = this.Publisher;
+        }
+
+        public int CompareTo(VideoGame? other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            return result;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name}, {Platform}, {Year}, {Genre}, {Publisher}, {NA_Sales}, {EU_Sales}, {JP_Sales}, {Other_Sales}, {Global_Sales}";
+        }
+
+    }
+}
+
